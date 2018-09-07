3fdd999eb1fc94e07c5890c25917d42d6a7c72e2
Arcangel Artigue <artigue_arcangel@hotmail.com>
2018-09-07 00:05:07 -0300

    add exerciseleven

--- Practica/ejerciciosavanzados_mvn/src/main/java/com/exerciseeleven/MovablePoint.java
+++ Practica/ejerciciosavanzados_mvn/src/main/java/com/exerciseeleven/MovablePoint.java
@@ -1,4 +1,44 @@
+/*
+ * Author: Arcangel Andres Artigue
+ * Email: artigue_arcangel@hotmail.com
+ *
+ * Copyright (c) 2018.
+ */
+
 package com.exerciseeleven;
 
-public class MovablePoint {
+public class MovablePoint implements Movable {
+
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public String toString() {
+        return "MovablePoint[x=" + x + ";y=" + y + ";xSpeed=" + xSpeed + ";ySpeed=" + ySpeed +"]";
+    }
+
+    public void moveUp() {
+
+    }
+
+    public void moveDown() {
+
+    }
+
+    public void moveRight() {
+
+    }
+
+    public void moveLeft() {
+
+    }
+
 }
